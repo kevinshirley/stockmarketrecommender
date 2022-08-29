64ce97d2ab84f5f3d0b83caa87d8325328d5711c
Kevin Shirley <kevinxshirley@gmail.com>
2022-08-29 04:51:35 -0400

    Head metadata

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import '../src/styles/app.scss'
 import React, { useEffect } from 'react';
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 import { wrapper } from '../src/store';
 import { useAction } from '../src/store/hooks';
 import * as actions from '../src/store/actions';
@@ -18,6 +19,12 @@ function Root({ Component, pageProps }: AppProps) {
 
   return (
     <main className={BEM_BLOCK}>
+      <Head>
+        <title>Stock Market Recommender | kevinshirley.com</title>
+        <meta name="description" content="App that can provide a buy, hold or sell recommendation when given a stock symbol. The recommendation adjusts itself based on data." />
+        <meta name="author" content="Kevin Shirley" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <Header />
       <Component {...pageProps} />
       <Footer />
